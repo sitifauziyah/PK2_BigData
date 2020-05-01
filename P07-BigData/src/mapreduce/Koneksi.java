f8ccb0bb08303e4fd0c9bbcc33576d317ef992fd
sitifauziyah <46235217+sitifauziyah@users.noreply.github.com>
2020-05-01 15:10:11 +0700

    Add files via upload

---
+++ P07-BigData/src/mapreduce/Koneksi.java
@@ -0,0 +1,22 @@
+package mapreduce;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Siti Fauziyah
+ */
+public class Koneksi {
+    public static MongoDatabase sambungDB() {
+        try {
+            MongoClient client = MongoClients.create();
+            MongoDatabase database = client.getDatabase("bigdata");
+            return database;
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+        }
+        return null;
+    }
+}
